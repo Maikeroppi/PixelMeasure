0a811421cc0cde8c9cd7dc68e9aaa6b0fafbf846
Michael Eads <maikeroppi@gmail.com>
2017-08-01 08:29:51 -0700

    Make transparency a little better
    -setDefaultAlphaBuffer seems to help a bit, but still a lot of weird
     tearing

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
-#include <QGuiApplication>
+#include <QApplication>
+#include <QMainWindow>
 #include <QQmlApplicationEngine>
+#include <QQuickWidget>
+#include <QQuickView>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QQuickWindow::setDefaultAlphaBuffer(true);
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
+
 
     return app.exec();
 }
--- main.qml
+++ main.qml
@@ -2,13 +2,20 @@ import QtQuick 2.7
 import QtQuick.Controls 2.0
 import QtQuick.Layouts 1.3
 
-ApplicationWindow {
+ApplicationWindow  {
     visible: true
     width: 640
     height: 480
 
-    flags: Qt.WA_TranslucentBackground | Qt.WindowStaysOnTopHint;
-    color: "#55880000";
+    flags: Qt.WA_TranslucentBackground // | Qt.FramelessWindowHint
+    color: "#00000000"
+
+    Rectangle {
+        anchors.fill: parent
+        border.width: 2;
+        border.color: "red"
+        color: "#00000000"
+    }
 }
 
 
